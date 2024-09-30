c68b84103e5ee2aaa29e36578245110f69d081a1
Mohd Umar <umohd9554@gmail.com>
2024-09-30 13:37:11 +0530

    Updaed mistakes

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ import './footer.css';
 const Footer = () => {
   return (
     <footer className='footer'>
-        Copyright &#169; 2023 Mohd Umar. All right received.
+        Copyright &#169; 2024 Mohd Umar. All right received.
     </footer>
   )
 }
